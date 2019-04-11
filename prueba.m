cb9ec24d8540a536be0a0645c6791c47ea100fb5
Oscar Rigoberto Carvajal Ortiz <or.carvajal.ortiz@gmail.com>
2019-04-10 22:08:21 -0700

    Update prueba.m

--- prueba.m
+++ prueba.m
@@ -1,17 +1,14 @@
-datos = load('redes2.mat');
-net = datos.arquitec.bestnnall(8).nn;
+clc
+clear all
+datos = load('redes.mat');
+net = datos.arquitec.bestnnall(108).nn;
 
-% load pacientesreal.dat
-% load tarreal.dat
-% 
-% x1=pacientesreal;
-% rtarg=tarreal;
 
 load pactst.dat
-load tarpactst.dat
+load tarpactstreal.dat
 
 x1=pactst;
-rtarg=tarpactst;
+rtarg=tarpactstreal;
 
 num=length(rtarg);
 %SIMULANDO DATOS PRONOSTICADOS
@@ -21,48 +18,18 @@ pn2 = normaliza(x1,7);
 
 an2 = sim(net,pn2);
 sim1 = desnormaliza(an2,1);
-sim2=round(sim1);
 
-% MSE= mymse(rtarg,sim2);
-% 
+
 
 %IMPRIMIR SOLO EL PRONOSITICO
-% pronostico1=round(rtarg);
 
-% perf = mse(net,pronostico1,sim2,'regularization',0.01);
-% perf=immse(pronostico1,sim2)
-% 
-%  for j=1:num
-%      pronostico1(1,j)=sim2(1,j);
-%  end
-%  
-%  pronostico1=round(pronostico1);
+MSE= mymse(rtarg,sim1);
 
-%%%%%%%%%%%%%%%%%%%%
-%INTEGRACION POR PROMEDIO
-% 
-% prom=pronostico1;
-% for iii=1:12
-%    erroresga(iii)=abs(rtarg(iii)-pronostico1(iii));  
-%    mse_calc(iii) = sum((rtarg(iii)-pronostico1(iii)).^2)/length(rtarg);
-% end
-
-% MSE = mean(( pronostico1 - rtarg).^2);
-MSE= mymse(rtarg,sim2);
-   %mse_calc = sum((y-targets).^2)/length(y);
-
-% errorestfpa=0;
-% for ii=1:12
-%    errorestfpa=errorestfpa+mse_calc(ii);
-% end
-% 
-% errorestfpa=errorestfpa/50
-
-p1 = 1:1:50;
+p1 = 1:1:numel(tarpactstreal);
 figure
-plot(p1,rtarg,'b');
-grid on
-hold on
-plot(p1,sim2,'r');
-hold on
 
+plot(p1,rtarg,'-ro',p1,sim1,'-.b');
+title(strcat('Experimento ALO con MSE : ',num2str(MSE)));
+xlabel('Pacientes')
+ylabel('% de riesgo')
+hleg1 = legend('Target','NN');
